2cbab450d7270a5484d3cd6e073ee5d89be6f092
EduardaSouzaOL <meso@cesar.school>
2025-10-28 12:33:00 -0300

    Revert "Fix: Importa modelo Feedback no admin"
    This reverts commit 11f4b074f7867ffeda9dda6f0da1e376afc7df3d.

--- jornal_commercio/admin.py
+++ jornal_commercio/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Noticia, Feedback
+from .models import Noticia
 
 @admin.register(Noticia)
 class NoticiaAdmin(admin.ModelAdmin):
