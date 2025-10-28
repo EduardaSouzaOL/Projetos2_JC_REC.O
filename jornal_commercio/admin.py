11f4b074f7867ffeda9dda6f0da1e376afc7df3d
sophiabritoa <sophia.brito@bidweb.com.br>
2025-10-28 09:40:17 -0300

    Fix: Importa modelo Feedback no admin

--- jornal_commercio/admin.py
+++ jornal_commercio/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Noticia
+from .models import Noticia, Feedback
 
 @admin.register(Noticia)
 class NoticiaAdmin(admin.ModelAdmin):
