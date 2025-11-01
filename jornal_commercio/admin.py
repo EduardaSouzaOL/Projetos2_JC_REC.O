3df733fe7397cbdf486ad403f1261753fd5ca56f
jarthur-duarte <joaoarthurdduarte@gmail.com>
2025-10-31 23:39:13 -0300

    "resolucao de error no site"

--- jornal_commercio/admin.py
+++ jornal_commercio/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Noticia
+from .models import Feedback
 
 @admin.register(Noticia)
 class NoticiaAdmin(admin.ModelAdmin):
