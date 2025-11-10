10426e3af9bdf3d0f03fc9dd7d38e8d14a445536
Caua Parente <cauahenriqueccpar@gmail.com>
2025-11-10 17:59:28 -0300

    Adicionado os campos de Comentario, Comunidade e Publicacao no painel de admin

--- jornal_commercio/admin.py
+++ jornal_commercio/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Noticia
 from .models import Feedback
+from .models import Comunidade
+from .models import Publicacao
+from .models import Comentario
 
 @admin.register(Noticia)
 class NoticiaAdmin(admin.ModelAdmin):
@@ -19,3 +22,41 @@ class NoticiaAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('titulo',)}
 
 admin.site.register(Feedback)
+
+@admin.register(Comunidade)
+class ComunidadeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'criador', 'data_criacao')
+    search_fields = ('nome', 'descricao')
+    list_filter = ('criador', 'data_criacao')
+    
+    # Melhora a interface de adicionar/remover membros
+    # (Transforma a lista de seleção em uma caixa de "filtro" horizontal)
+    filter_horizontal = ('membros',) 
+
+
+# Esta classe 'inline' permite ver e editar Comentários
+# DIRETAMENTE de dentro da página de uma Publicação. É muito útil!
+class ComentarioInline(admin.TabularInline):
+    model = Comentario
+    fields = ('autor', 'conteudo', 'data_publicacao')
+    readonly_fields = ('data_publicacao',) # Não deixa editar data automática
+    extra = 0 # Não mostra campos extras para novos comentários por padrão
+
+
+@admin.register(Publicacao)
+class PublicacaoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'autor', 'comunidade', 'data_publicacao', 'is_destaque')
+    list_filter = ('comunidade', 'autor', 'is_destaque')
+    search_fields = ('conteudo', 'autor__username', 'comunidade__nome')
+    
+    # Melhora a interface para 'curtidas' e 'salvo_por'
+    filter_horizontal = ('curtidas', 'salvo_por')
+    
+    # Adiciona o 'inline' de comentários na página da publicação
+    inlines = [ComentarioInline]
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('autor', 'publicacao', 'data_publicacao')
+    list_filter = ('autor', 'data_publicacao')
+    search_fields = ('conteudo', 'autor__username')
