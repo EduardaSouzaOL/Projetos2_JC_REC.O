e61d8c62928f5f040c8a0b6ef34959f93c743479
jarthur-duarte <joaoarthurdduarte@gmail.com>
2025-11-16 17:21:10 -0300

    "correcao entrypoint.sh"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+set -e 
+
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 
 cd /home/site/wwwroot
 
+echo "A executar migrações (migrate)..."
 /home/site/wwwroot/antenv/bin/python manage.py migrate --noinput
 
+echo "Migrações concluídas. A iniciar o Gunicorn..."
 gunicorn --bind=0.0.0.0 --workers=4 --timeout 120 project.wsgi
