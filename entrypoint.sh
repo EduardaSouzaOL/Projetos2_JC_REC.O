5aca573f19f6e682405c63cd6648702fd41cc5c4
jarthur-duarte <joaoarthurdduarte@gmail.com>
2025-11-16 16:40:46 -0300

    mudancas no entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
-gunicorn --bind=0.0.0.0 --workers=4 project.wsgi
+gunicorn --bind=0.0.0.0 --workers=4 --timeout 120 project.wsgi
