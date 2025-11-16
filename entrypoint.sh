3cb2c10280f7e0fafe91dee5916e36fd54f3f9e9
jarthur-duarte <joaoarthurdduarte@gmail.com>
2025-11-16 16:53:31 -0300

    "mudancas entrypoint.sh"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+/home/site/wwwroot/antenv/bin/python manage.py migrate --noinput
 gunicorn --bind=0.0.0.0 --workers=4 --timeout 120 project.wsgi
